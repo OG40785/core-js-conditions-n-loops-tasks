61360f7c7730916fee9d8bc1a36b4b405bdeaf31
Olga G <og.40785@gmail.com>
2024-12-29 23:16:05 +0100

    add:10 test passing

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -71,8 +71,17 @@ function getMaxNumber(a, b, c) {
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  */
-function canQueenCaptureKing(/* queen, king */) {
-  throw new Error('Not implemented');
+function canQueenCaptureKing(queen, king) {
+  if (queen.x === queen.y && king.x === king.y) {
+    return true;
+  }
+  if (queen.x === king.x || queen.y === king.y) {
+    return true;
+  }
+  if (queen.x - king.x === king.y - queen.y) {
+    return true;
+  }
+  return false;
 }
 
 /**
